305a3fa0f7827402ec86e805ec8d71acac2a5dcf
mikebvs <mikeb.vansickle@gmail.com>
2025-02-17 22:42:16 -0500

    Added player draw and movement functionality

--- constants.py
+++ constants.py
@@ -1,7 +1,14 @@
+# Pygame Constants
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+# Asteroid Constants
 ASTEROID_MIN_RADIUS = 20
 ASTEROID_KINDS = 3
 ASTEROID_SPAWN_RATE = 0.8  # seconds
 ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+
+# Player Constants
+PLAYER_RADIUS = 20
+PLAYER_TURN_SPEED = 300  # degrees per second
+PLAYER_SPEED = 200  # pixels per second
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame # https://www.pygame.org/docs/ref/pygame.html
 from constants import *
+from player import Player
 
 def main():
     print(f"Starting asteroids! Running resolution: {SCREEN_WIDTH}x{SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("Exiting asteroids!")
                 return
+            
+        dt = clock.tick(60)/1000
         screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
 
